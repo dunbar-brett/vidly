0fbdb8f48943055f44a61b350dbf7557d9c62b8b
Brett <Brett@BRETT-PC>
2018-01-02 23:04:17 -0700

    cleaned up the rental api controller, created newRentalDto

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRental(NewRentalDto newRental)
+        {
+            return Ok();
+        }
+    }
+}
